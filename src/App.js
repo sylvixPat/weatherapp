b7899e05e8eccf7297e6c519da3b4eda67d58bbe
sylvixPat <sylvi86@gmail.com>
2024-08-08 20:12:51 -0500

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="">
       <h1 className="bg-slate-200 text-2xl flex">Weather App</h1>
+      <p>Nuevo proyecto en grupo Doris, Pauli y Sylvi</p>
     </div>
   );
 }
